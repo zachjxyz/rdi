e8f4b97e25500676e9202abb0cf0271080e79b37
Zach <73715397+ZachVJ@users.noreply.github.com>
2024-03-21 20:46:00 -0400

    idk what happened to the js lol

--- js/ghost.js
+++ js/ghost.js
@@ -1,4 +1,4 @@
-const url = "https://blog.realdealindividual.com/ghost/api/content/posts/?key=f7f2fd774e39bca33f0611373b";
+const url = "https://blog.realdealindividual.com/ghost/api/content/posts/?key=316fe13cdab156d407ae990a77";
 
 document.addEventListener("DOMContentLoaded", function(event) {
     fetch(url)
@@ -6,26 +6,24 @@ document.addEventListener("DOMContentLoaded", function(event) {
         return res.json();
     })
     .then(data => {
+        num_of_blogs = Object.keys(data).length;
 
-        let numOfBlogs = Object.keys(data).length;
+        blog_posts = document.getElementById('post-content');
 
-        let blogPosts = document.getElementById('post-content');
-
-        for (i = numOfBlogs; i >= 0; i--) {
-
+        for (i = num_of_blogs - 1; i >= 0; i--) {
             let blog_title = data.posts[i].title;
             let blog_image = data.posts[i].feature_image;
             let blog_excerpt = data.posts[i].custom_excerpt;
             let blog_link = data.posts[i].url;
-            let blog_published_date = Date.parse(data.posts[i].published_at);
+            let blog_date = Date.parse(data.posts[i].published_at);
 
-            var date = new Date(blog_published_date);
+            let date = new Date(blog_date);
 
             let blog_year = date.getFullYear();
             let blog_month = (date.getMonth() + 1).toString().padStart(2, "0");
             let blog_day = date.getUTCDate();
 
-            blogPosts.insertAdjacentHTML("afterbegin", 
+            blog_posts.insertAdjacentHTML("afterbegin", 
                 `<article class="entry-item">
                     <div class="entry-img">
                         <a href="${blog_link}" target="_blank">
