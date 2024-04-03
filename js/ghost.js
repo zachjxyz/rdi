fb20ed359d57676460ae92fdc36b9ead5c82400c
Zach <73715397+ZachVJ@users.noreply.github.com>
2024-04-03 13:08:03 -0400

    updated ghost script

--- js/ghost.js
+++ js/ghost.js
@@ -1,68 +1,56 @@
-const url = "https://blog.realdealindividual.com/ghost/api/content/posts/?key=316fe13cdab156d407ae990a77&include=authors";
+const api = new GhostContentAPI({
+    url: 'https://blog.realdealindividual.com',
+    key: '316fe13cdab156d407ae990a77',
+    version: 'v5.0'
+});
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    fetch(url)
-    .then(res => {
-        return res.json();
-    })
-    .then(data => {
-        idx_of_blogs = Object.keys(data.posts).length - 1;
+api.posts.browse({
+    filter: 'tag:rdi-nights',
+    include: 'authors'
+})
+.then((obj) => {
+    let posts = obj.reverse();
 
-        blog_posts = document.getElementById('post-content');
+    blog_posts = document.getElementById('post-content');
 
-        for (i = idx_of_blogs; i >= 0; i--) {
-            let blog_id = data.posts[i].id;
-            let blog_html = data.posts[i].html;
-            let blog_slug = data.posts[i].slug;
-            let blog_author = data.posts[i].authors[i].name;
-            let blog_title = data.posts[i].title;
-            let blog_image = data.posts[i].feature_image;
-            let blog_excerpt = data.posts[i].custom_excerpt;
-            let blog_link = data.posts[i].url;
-            let reading_time = data.posts[i].reading_time;
-            let blog_date = Date.parse(data.posts[i].published_at);
+    obj.forEach((post) => {
+        let blog_date = new Date(Date(post.published_at));
 
-            let date = new Date(blog_date);
+        let blog_month = blog_date.getUTCMonth();
+        let blog_day = blog_date.getUTCDate();
+        let blog_year = blog_date.getUTCFullYear();
 
-            let blog_year = date.getFullYear();
-            let blog_month = (date.getMonth() + 1).toString().padStart(2, "0");
-            let blog_day = date.getUTCDate();
-            
-            blog_posts.insertAdjacentHTML("afterbegin", 
-                `<article class="entry-item">
-                    <div class="entry-img">
-                        <a href="${blog_link}" id="create-blog">
-                        <img src="${blog_image}" alt="${blog_title}">
-                        </a>
+        blog_posts.insertAdjacentHTML("afterbegin", 
+        `<article class="entry-item">
+                <div class="entry-img">
+                    <a href="${post.url}" target="_blank" id="create-blog">
+                    <img src="${post.feature_image}" alt="${post.title}">
+                    </a>
+                </div>
+                <div class="entry-wrap">
+                    <div class="entry">  
+                    <h2 class="entry-title">${blog_month}/${blog_day}/${blog_year}: ${post.title}
+                    </h2>
+                    <ul class="entry-meta">
+                        <li class="entry-author">
+                            <i class="fa fa-user"></i>
+                            ${post.primary_author.name}
+                        </li> 
+                        <li class="entry-date">
+                            <i class="fa fa-book"></i>
+                            ${post.reading_time} mins
+                        </li>
+                    </ul>
+                    <div class="entry-content">
+                        <p>${post.custom_excerpt}</p>
+                        <a href="${post.url}" target="_blank" class="btn btn-lg btn-stroke read-more-btn" id="create-blog"><span>Read More</span></a>
                     </div>
-                    <div class="entry-wrap">
-                        <div class="entry">  
-                        <h2 class="entry-title">${blog_month}/${blog_day}/${blog_year}: ${blog_title}
-                        </h2>
-                        <ul class="entry-meta">
-                            <li class="entry-author">
-                                <i class="fa fa-user"></i>
-                                ${blog_author}
-                            </li> 
-                            <li class="entry-date">
-                                <i class="fa fa-book"></i>
-                                ${reading_time} mins
-                            </li>
-                        </ul>
-                        <div class="entry-content">
-                            <p>${blog_excerpt}</p>
-                            <a href="${blog_link}" class="btn btn-lg btn-stroke read-more-btn" id="create-blog"><span>Read More</span></a>
-                        </div>
-                        </div>
                     </div>
-                </article>`
-            );
-
-            // document.getElementById("create-blog").addEventListener("click", function(event) {
-            //     document.getElementById("create-blog").setAttribute('href', `${blog_link}`
-            //     );
-            // });
-        }
+                </div>
+            </article>`
+        )
     })
-    .catch(error  => console.log(error));
-  });
+})
+.catch((error)  => { 
+    console.log(error);
+});
